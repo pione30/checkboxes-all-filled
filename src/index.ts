28b9fec5c748e9a070e29fc2f5850bc71fe54b6f
pione30 <g.pionelish30@gmail.com>
2020-12-22 19:13:38 +0900

    [wip] log the body of a pull_request

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,17 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
+  if (github.context.eventName !== "pull_request") {
+    throw new Error("This action must be triggered by `pull_request` event.");
+  }
+
+  if (github.context.payload.pull_request === undefined) {
+    throw new Error(
+      "The requested payload seems NOT to be one of the Pull Request API."
+    );
+  }
+
+  core.info(github.context.payload.pull_request.body ?? "");
 } catch (error) {
   core.setFailed(error.message);
 }
